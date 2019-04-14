469b35d409a580d143881987c95f57b920b0fb99
raphapfeifer <rapha_pfeifer@yahoo.com.br>
2019-04-14 02:17:14 -0300

    14/04/2019

---
+++ java8/src/java8/OrdenaStrings.java
@@ -0,0 +1,65 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+		
+		
+		List<String> palavras = new ArrayList<String>();
+		palavras.add("alura online");
+		palavras.add("editora casa do código");
+		palavras.add("caelum");
+		
+		Comparator<String> comparator = new ComparatorPorTamanho(); 
+		
+		
+		//Collections.sort(palavras,comparator);
+		
+		palavras.sort(comparator);
+		System.out.println(palavras);
+
+		
+		//Consumer<String> consumidor = new ImprimeNaLinha();
+		//palavras.forEach(consumidor);
+		
+	}
+
+}
+
+
+class ImprimeNaLinha implements Consumer<String>{
+
+	@Override
+	public void accept(String s) {
+		System.out.println(s);
+	}
+	
+	
+	
+}
+		
+class ComparatorPorTamanho implements Comparator<String>{
+
+	@Override
+	public int compare(String s1, String s2) {
+		
+		if(s1.length() < s2.length()) {
+			return -1;
+		}
+					
+		if(s1.length() > s2.length()) {
+			return 1;
+		}
+		
+		return 0;
+	}
+	
+	
+	
+}
